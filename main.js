1c6d0b272b719c6f5ae2e3070ef1234198acdb9a
Ninjabear <83464653+igrady2@users.noreply.github.com>
2021-11-01 14:46:13 -0400

    removed changeText from main.js and added as script in index.html

--- main.js
+++ main.js
@@ -35,21 +35,16 @@ button6.addEventListener('click', event => {
 }); 
 // sets Adds Event listener to reset button, randomizes mystery numbers, and resets background default.   
 
-function changeText(id,num) {
- id.innerHTML = arr[num];
-} //Called in HTML lines 40-42
+//Called in HTML lines 40-42
 
 button3.addEventListener('click', event => {
   buttonArea3.style.backgroundColor = arr[1];
-  changeText(button3,1);
 }); 
 button4.addEventListener('click', event => {
   buttonArea3.style.backgroundColor = arr[2];
-  changeText(button4,2);
 });
 button5.addEventListener('click', event => {
   buttonArea3.style.backgroundColor = arr[3];
-  changeText(button5,3);
 });
 // adds listeners to buttons 3-5.  
 
