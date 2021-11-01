1bc87804ae5ee897ac41347b0cbec2226f7a1828
Ninjabear <83464653+igrady2@users.noreply.github.com>
2021-11-01 15:43:06 -0400

    add ';' after return line for getRGB

--- modules/helperfunctions.js
+++ modules/helperfunctions.js
@@ -2,12 +2,12 @@ const getRGB = () => {
   let rRandom = Math.floor(Math.random * 255);
   let gRandom = Math.floor(Math.random * 255);
   let bRandom = Math.floor(Math.random * 255);
-  return `rgb(${rRandom},${gRandom},${bRandom})`
+  return `rgb(${rRandom},${gRandom},${bRandom})`;
 }
 
 const addListenerToChangeColor = (button,target,att,color1) => {
   return button.addEventListener('click', event => {
-    target.style.att = color1
+    target.style.att = color1;
   });
 }
 
